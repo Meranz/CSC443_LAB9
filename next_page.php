57d07764c8d347e441af0f92aaaba355db7c2940
Meranz <2020813654@student.uitm.edu.my>
2022-06-08 02:36:22 +0800

    next_page.php

---
+++ next_page.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+echo "<html>";
+
+
+$queryString = $_SERVER["QUERY_STRING"];
+echo "Query string of the incoming URL: ".$queryString ."<br>";
+
+echo "Cookies received: <br>";
+foreach ($_COOKIE as $name => $value)
+{ 
+    echo " $name = $value <br>";
+}
+
+
+$myLogin = $_SESSION["myLogin"];
+echo "Value of myLogin has been retrieved: ".$myLogin. "<br>";
+
+$myColor = $_SESSION["myColor"];
+echo "Value of MyColor has been retrieved: ".$myColor."<br>";
+
+echo "</html><br>";
+
+?>
